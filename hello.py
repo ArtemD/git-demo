73437f005599bc9a941cee3772d5858cafbfc03f
Artem Danilaints <artem@daniliants.com>
2022-03-11 14:08:53 +0200

    Added better comments thanks to co-pilot

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 from flask import Flask
 
+# Create the application object
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """ Main page """
     return 'Web App with Python Flask!'
 
+# Run the application in debug mode
 app.run(host='0.0.0.0', port=8000)
