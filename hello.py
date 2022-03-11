1ee38136a2616ad04fede11436aae74faadba712
Artem Danilaints <artem@daniliants.com>
2022-03-11 14:11:23 +0200

    Added simple API endpoint

--- hello.py
+++ hello.py
@@ -8,5 +8,10 @@ def index():
     """ Main page """
     return 'Web App with Python Flask!'
 
+@app.route('/api/v1/hello')
+def hello():
+    """ Hello World page """
+    return 'Hello World!'
+
 # Run the application in debug mode
 app.run(host='0.0.0.0', port=8000)
